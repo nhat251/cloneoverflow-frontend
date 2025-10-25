95046f1813d00f1408bc5c9902c9df7e3bd8ecc7
nhat251 <nsn251tp@gmail.com>
2025-10-25 22:10:07 +0700

    init project voi em khang

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      {/* <ThemeProvider theme={theme}> */}
+      {/* <CssBaseline /> */}
+      <BrowserRouter>
+        <h1>Hello</h1>
+      </BrowserRouter>
+      {/* </ThemeProvider> */}
+    </>
+  );
+}
+
+export default App;
