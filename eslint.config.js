79c8458046ec9657392651a738bd2bf2e2bbb562
nhat251 <nsn251tp@gmail.com>
2025-10-26 10:05:10 +0700

    fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,30 +3,18 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-import prettier from 'eslint-config-prettier';
-import eslintPluginPrettier from 'eslint-plugin-prettier';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx,js,jsx}'],
-    ignores: ['node_modules', 'dist'],
+    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
-      prettier, // Tắt rule format của ESLint để nhường cho Prettier
     ],
-    plugins: {
-      prettier: eslintPluginPrettier,
-    },
-    rules: {
-      'prettier/prettier': 'warn', // Cảnh báo nếu format không đúng Prettier
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
