bafba3dc02e46c1b0ec5f2865cf6e73448a206b7
nhat251 <nsn251tp@gmail.com>
2025-10-31 19:44:12 +0700

    css notice bar

--- src/components/layouts/components/Notice/index.tsx
+++ src/components/layouts/components/Notice/index.tsx
@@ -9,17 +9,21 @@ const cx = classNames.bind(styles);
 function Notice() {
   return (
     <div className={cx('wrapper')}>
-      <div className={cx('notice-inner')}>
-        <div className={cx('notice-logo')}>
-          <img src={Facebook_logo} alt="Facebook" />
-        </div>
-        <div className={cx('notice-text')}>
-          <p>
-            Cùng tham gia <strong style={{ color: 'red' }}>nhóm</strong> của <strong>FuOverflow</strong> trên Facebook
-            www.facebook.com/groups/fuoverflow
-          </p>
-        </div>
-      </div>
+      <ul className={cx('notice-inner')}>
+        <li className={cx('notice-content')}>
+          <div className={cx('notice-logo')}>
+            <img src={Facebook_logo} alt="Facebook" />
+          </div>
+          <div className={cx('notice-text')}>
+            <p>
+              Cùng tham gia <strong style={{ color: 'red' }}>nhóm</strong> của <strong>FuOverflow</strong> trên Facebook{' '}
+              <a href="https://facebook.com/groups/fuoverflow" target="_blank">
+                <strong style={{ color: 'hsla(223, 63%, 52%, 1)' }}>www.facebook.com/groups/fuoverflow</strong>
+              </a>
+            </p>
+          </div>
+        </li>
+      </ul>
     </div>
   );
 }
